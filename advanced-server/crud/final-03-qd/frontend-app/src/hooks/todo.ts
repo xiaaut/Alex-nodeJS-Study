dbf9cc56d70865ec89ff233e59b5e2f3bd71eaeb
xia <836807919@qq.com>
2026-01-06 01:28:55 +0800

    完善查询和删除

--- advanced-server/crud/final-03-qd/frontend-app/src/hooks/todo.ts
+++ advanced-server/crud/final-03-qd/frontend-app/src/hooks/todo.ts
@@ -1,48 +1,44 @@
-import { useEffect, useState } from 'react';
-import { Todo } from '../types/Todo';
-import { deleteTodoById, getTodos } from '../service/apiTodo';
+import { useEffect, useState } from 'react'
+import { Todo } from '../types/Todo'
+import { deleteTodoById, getTodos } from '../service/apiTodo'
 
 export function useTodo() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([])
 
   async function deleteTodo(id: number) {
-    // await deleteTodoById(id);
+    await deleteTodoById(id)
 
-    setTodos(todos.filter((todo: Todo) => todo.id !== id));
+    setTodos(todos.filter((todo: Todo) => todo.id !== id))
   }
 
   function addTodo(todo: Todo) {
-    setTodos([...todos, todo]);
+    setTodos([...todos, todo])
   }
 
   function updateTodo(
     todoId: number,
     updateTodo: {
-      title?: string;
-      tag?: string;
+      title?: string
+      tag?: string
     }
   ) {
-    setTodos(
-      todos.map((todo: Todo) =>
-        todo.id === todoId ? { ...todo, ...updateTodo } : todo
-      )
-    );
+    setTodos(todos.map((todo: Todo) => (todo.id === todoId ? { ...todo, ...updateTodo } : todo)))
   }
 
   useEffect(() => {
     async function loadData() {
-      const todosData = await getTodos();
+      const todosData = await getTodos()
 
-      setTodos(todosData);
+      setTodos(todosData)
     }
 
-    loadData();
-  }, []);
+    loadData()
+  }, [])
 
   return {
     todos,
     deleteTodo,
     addTodo,
     updateTodo,
-  };
+  }
 }
--- advanced-server/crud/final-04-cu/backend-app/server.js
+++ advanced-server/crud/final-04-cu/backend-app/server.js
@@ -12,8 +12,7 @@ app.get('/todos', async (_req, res) => {
   const todosData = await readFile('./data.json', 'utf-8');
   const todos = JSON.parse(todosData);
 
-  console.log(todos);
-
+  // console.log(todos);
 
   return res.status(200).json(todos);
 });
@@ -24,14 +23,14 @@ app.get('/todos/:todoId', async (req, res) => {
 
   const todoId = req.params.todoId;
 
-  const todo = todos.find(todo => todo.id === Number(todoId));
+  const todo = todos.find((todo) => todo.id === Number(todoId));
 
   if (todo) {
     return res.status(200).json(todo);
   }
 
   return res.status(404).send('404 Not Found');
-})
+});
 
 app.get('/todos/delete/:todoId', async (req, res) => {
   const todosData = await readFile('./data.json', 'utf-8');
@@ -39,15 +38,16 @@ app.get('/todos/delete/:todoId', async (req, res) => {
 
   const todoId = req.params.todoId;
 
-  const filteredTodos = todos.filter(todo => todo.id !== Number(todoId));
+  const filteredTodos = todos.filter((todo) => todo.id !== Number(todoId));
 
   await writeFile('./data.json', JSON.stringify(filteredTodos), 'utf-8');
+
+  // console.log(todos);
 
   return res.status(200).json({
     message: 'Todo deleted successfully',
   });
-})
-
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
